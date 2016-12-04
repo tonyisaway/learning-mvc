0303113d35f7bae6a86bf0963fd6bddcee3b513c
Tony Cooke <tonyisaway@hotmail.com>
2016-12-04 22:37:42 +0000

    CRUD with MVC, Angular and WebAPI

---
+++ LearningMvc/AngularJsDemo/Scripts/ProductService.js
@@ -0,0 +1,56 @@
+﻿(function () {
+
+    var app = angular.module("productsapp");
+
+    var ProductsService = function (http) {
+
+        var productBaseUrl = "/api/Product/";
+        var productsBaseUrl = "/api/Products/";
+
+        var getProducts = function () {
+            return http.get(productsBaseUrl + "Get")
+                .then(function (response) {
+                    return response.data;
+                });
+        };
+
+        var getProduct = function (id) {
+            return http.get(productBaseUrl + id)
+                .then(function (response) {
+                    return response.data;
+                });
+        };
+
+        var deleteProduct = function (id) {
+            return http.delete(productBaseUrl + id)
+                .then(function (response) {
+                    return response.data;
+                });
+        };
+
+        var updateProduct = function (product) {
+            return http.put(productBaseUrl + product.Id, product)
+                .then(function (response) {
+                    return response.data;
+                });
+        };
+
+        var addProduct = function (product) {
+            return http.post(productBaseUrl, product)
+                .then(function (response) {
+                    return response.data;
+                });
+        };
+
+        return {
+            getProducts: getProducts,
+            getProduct: getProduct,
+            deleteProduct: deleteProduct,
+            updateProduct: updateProduct,
+            addProduct: addProduct
+        };
+    };
+
+    app.factory("productsService", ["$http", ProductsService]);
+
+})();
