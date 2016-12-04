b40295a535fe193446f32c0831acab12273a1c63
Tony Cooke <tonyisaway@hotmail.com>
2016-12-04 12:31:47 +0000

    Understanding how MVC works.

---
+++ LearningMvc/DemoMVC/Controllers/HomeController.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using DemoMVC.Models;
+
+namespace DemoMVC.Controllers
+{
+    public class HomeController : Controller
+    {
+        //site/home/index
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        //site/home/about
+        public ActionResult About()
+        {
+            ViewBag.Message = "";
+
+            return View();
+        }
+
+        [HttpPost]
+        public ActionResult About(string userData)
+        {
+            ViewBag.Message = userData;
+
+            return View();
+        }
+
+        // site/home/list
+        public ActionResult List()
+        {
+            return View(TechItem.GetItems());
+        }
+
+        //site/home/contact
+        public ActionResult Contact()
+        {
+            ViewBag.Message = "Your contact page.";
+
+            return View();
+        }
+    }
+}
